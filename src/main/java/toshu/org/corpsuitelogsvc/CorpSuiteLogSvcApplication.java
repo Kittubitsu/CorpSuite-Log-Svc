3fcff8e7337b07117846d65d1e4ce4e38c7ca7ad
kittu <kittubitsu@gmail.com>
2025-03-09 08:51:32 +0200

    initial commit

---
+++ src/main/java/toshu/org/corpsuitelogsvc/CorpSuiteLogSvcApplication.java
@@ -0,0 +1,13 @@
+package toshu.org.corpsuitelogsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpSuiteLogSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorpSuiteLogSvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toshu/org/corpsuitelogsvc/CorpSuiteLogSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package toshu.org.corpsuitelogsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorpSuiteLogSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
