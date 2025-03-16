5008a3185f504b0f7b5710752d65d2d75aafc495
kittu <kittubitsu@gmail.com>
2025-03-16 15:35:44 +0200

    cleared unused code

--- src/main/java/toshu/org/corpsuitelogsvc/web/LogController.java
+++ src/main/java/toshu/org/corpsuitelogsvc/web/LogController.java
@@ -3,7 +3,6 @@ package toshu.org.corpsuitelogsvc.web;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import toshu.org.corpsuitelogsvc.model.Log;
 import toshu.org.corpsuitelogsvc.service.LogService;
