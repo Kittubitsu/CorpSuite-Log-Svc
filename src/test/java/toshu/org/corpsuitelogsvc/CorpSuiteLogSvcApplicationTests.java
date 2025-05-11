505ef53b3d73320225441d7dbfb722b75cefe607
Toshu <kittubitsu@gmail.com>
2025-05-11 19:14:38 +0300

    fixed test properties file

--- src/test/java/toshu/org/corpsuitelogsvc/CorpSuiteLogSvcApplicationTests.java
+++ src/test/java/toshu/org/corpsuitelogsvc/CorpSuiteLogSvcApplicationTests.java
@@ -1,7 +1,9 @@
 package toshu.org.corpsuitelogsvc;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import toshu.org.corpsuitelogsvc.repository.LogRepository;
 
 @SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 class CorpSuiteLogSvcApplicationTests {
